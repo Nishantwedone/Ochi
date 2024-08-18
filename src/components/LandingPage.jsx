893f6a6cb7213a19c85146ac370fde1b076a7686
Nishant Kumar Gupta <nishantkumargupta728@gmail.com>
2024-08-19 01:50:12 +0530

    add some animations

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,17 +1,26 @@
+import { motion } from 'framer-motion';
 import React from 'react'
 // arrow import below
 import { FaArrowUp } from "react-icons/fa6";
 
 function LandingPage() {
+
   return (
-    <div className='w-full h-screen bg-zinc-900 pt-1'>
+    <div data-scroll data-scroll-section data-scroll-speed="-.3" className='w-full h-screen bg-zinc-900 pt-1'>
         <div className='textstructure mt-52 px-20'>
             {["we create","eye-opening","presentations"].map((item,index) =>{
                 return (
             <div className="masker">
                 <div className='w-fit flex items-center'>
                     {/* {index===1 && (<div className='w-[6.5vw] h-[5vw] relative bg-red-500'></div>)} */}
-                    {index===1 && (<div className='mr-[1vw] w-[7vw] rounded-md h-[5vw] relative bg-red-500'></div>)}
+                    {index===1 && (
+                        <motion.div 
+                        initial={{width:0}} 
+                        animate={{width:"7vw"}} 
+                        transition={{ease:[0.76, 0, 0.24, 1], duration:1}} 
+                        className='mr-[1vw] w-[7vw] rounded-md h-[5vw] relative bg-[url("https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-1440x921.jpg")] bg-cover bg-center'>
+
+                        </motion.div>)}
                     
                 <h1 className='flex items-center uppercase text-[6.5vw] h-full leading-[6vw] tracking-tighter font-["Founders Grotesk"] font-medium'>{item}</h1>
                 {/* <h1 className='pt-[2vw] -mb-[1vw] uppercase text-[8.5vw] leading-[.7]  font-["Founders Grotesk"]'>{item}</h1> */}
--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -4,15 +4,15 @@ import React from 'react'
 function Marquee() {
     
   return (
-    <div className='w-full py-20 rounded-tl-3xl rounded-tr-3xl bg-[#004D43]'>
-        <div className='text border-t-2 border-b-2 border-zinc-300 flex whitespace-nowrap gap-10 overflow-hidden'>
-            <motion.h1 initial={{x: 0}} animate={{x: "-100%"}} transition={{ease: "linear", repeat: Infinity, duration: 5}} className='text-[15vw] leading-none font-["Founders Grotesk"] uppercase font-semibold '>we are oochi</motion.h1>
+    <div data-scroll data-scroll-section data-scroll-speed=".1" className='w-full py-20 rounded-tl-3xl rounded-tr-3xl bg-[#004D43]'>
+        <div className='text border-t-2 border-b-2 border-zinc-300 flex whitespace-nowrap overflow-hidden'>
+            <motion.h1 initial={{x: 0}} animate={{x: "-100%"}} transition={{ease: "linear", repeat: Infinity, duration: 5}} className='text-[15vw] leading-none pr-10 font-["Founders Grotesk"] uppercase font-semibold '>we are oochi</motion.h1>
 
             <motion.h1 initial={{x: 0}} animate={{x: "-100%"}} transition={{ease: "linear", repeat: Infinity, duration: 5}} className='text-[15vw] leading-none font-["Founders Grotesk"] uppercase font-semibold '>we are oochi</motion.h1>
             
             <motion.h1 initial={{x: 0}} animate={{x: "-100%"}} transition={{ease: "linear", repeat: Infinity, duration: 5}} className='text-[15vw] leading-none font-["Founders Grotesk"] uppercase font-semibold '>we are oochi</motion.h1>
             
-            <motion.h1 initial={{x: 0}} animate={{x: "-100%"}} transition={{ease: "linear", repeat: Infinity, duration: 5}} className='text-[15vw] leading-none font-["Founders Grotesk"] uppercase font-semibold '>we are oochi</motion.h1>
+            {/* <motion.h1 initial={{x: 0}} animate={{x: "-100%"}} transition={{ease: "linear", repeat: Infinity, duration: 5}} className='text-[15vw] leading-none font-["Founders Grotesk"] uppercase font-semibold '>we are oochi</motion.h1>  */}
             
         </div>
     </div>
